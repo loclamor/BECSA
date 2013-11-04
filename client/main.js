c2bd1b5c80f9d68393f632efa685e01bc24de9ec
loclamor <favier.loic.31@gmail.com>
2013-11-04 20:09:04 +0100

    Create main JS structure

--- client/main.js
+++ client/main.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
+    main();
     /**
      * clic sur une fonction
      */
     $("button.btn-function").click(function(){
         $("button.btn-function").removeClass("active");
         $(this).addClass("active");
+        switch ( $(this).data("function") ) {
+            case "lumiere":
+                lumiere();
+            break;
+            case "volet":
+                volet();
+            break;
+            case "porte":
+                porte();
+            break;
+            case "hifi":
+                hifi();
+            break;
+            case "reveil":
+                reveil();
+            break;
+            case "itineraire":
+                itineraire();
+            break;
+            case "meteo":
+                meteo();
+            break;
+            default :
+                main();
+        }
     });
+    
 });
+
+function main(){
+    $("#fctTitle").html("Bonjour");
+    $("#fctBody").html("Pour commencer, choisissez une fonctionnalité.");
+}
