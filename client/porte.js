6f7b7f3a89aae4aaf284bc67af822f7cc0b15b84
loclamor <favier.loic.31@gmail.com>
2013-11-07 09:40:52 +0100

    add porte and volet functions page

---
+++ client/porte.js
@@ -0,0 +1,47 @@
+function porte() {
+    $("#fctTitle").html("Portes");
+    var body = $("#fctBody");
+    body.html("");
+    body.removeClass();
+    body.addClass("porte");
+    addOnOffSwitcher( body, "all", "Toutes les portes", "Déverrouiller", "Verrouiller",
+        getControllerActionUrl("porte", "deverrouillerTout"),
+        getControllerActionUrl("porte", "verrouillerTout"),
+        "no",
+        refreshPiecePorte
+    );
+    
+    $.getJSON( getControllerActionUrl("porte", "lister"), function( data ){
+        $.each( data.pieces, function( key, val ) {
+            addOnOffSwitcher( body, val.id, val.nom, "Déverrouiller", "Verrouiller",
+                getControllerActionUrl("porte", "deverrouiller", val.id),
+                getControllerActionUrl("porte", "verrouiller", val.id),
+                val.porteVerrouillee ? "off" : "on",
+                refreshPiecePorte
+            );
+        });
+    });
+}
+
+/**
+ * Refresh a onOffSwitcher for a Piece added with addOnOffSwitcher(...)
+ * @param {Piece} piece
+ * @returns {void}
+ */
+function refreshPiecePorte( piece ){
+        //change imgState
+        var eltImgState = $("#onOffRow_" + piece.id + " .imgState");
+        if( !eltImgState.hasClass("no") ){
+            eltImgState.removeClass( piece.porteVerrouillee ? "on" : "off" );
+            eltImgState.addClass( piece.porteVerrouillee ? "off" : "on" );
+        }
+        
+        var eltBtnOn = $("#onOff_"+piece.id+"On");
+        var eltBtnOff = $("#onOff_"+piece.id+"Off");
+        if( eltBtnOn.hasClass("hidden") || eltBtnOff.hasClass("hidden") ){
+            eltBtnOn.removeClass("hidden");
+            eltBtnOff.removeClass("hidden");
+            eltBtnOn.addClass( piece.porteVerrouillee ? "" : "hidden" );
+            eltBtnOff.addClass( piece.porteVerrouillee ? "hidden" : "" );
+        }
+    }
---
+++ client/volet.js
@@ -0,0 +1,47 @@
+function volet() {
+    $("#fctTitle").html("Volets");
+    var body = $("#fctBody");
+    body.html("");
+    body.removeClass();
+    body.addClass("volet");
+    addOnOffSwitcher( body, "all", "Tous les volets", "Ouvrir", "Fermer",
+        getControllerActionUrl("volet", "ouvrirTout"),
+        getControllerActionUrl("volet", "fermerTout"),
+        "no",
+        refreshPieceVolet
+    );
+    
+    $.getJSON( getControllerActionUrl("volet", "lister"), function( data ){
+        $.each( data.pieces, function( key, val ) {
+            addOnOffSwitcher( body, val.id, val.nom, "Ouvrir", "Fermer",
+                getControllerActionUrl("volet", "ouvrir", val.id),
+                getControllerActionUrl("volet", "fermer", val.id),
+                val.voletOuvert ? "on" : "off",
+                refreshPieceVolet
+            );
+        });
+    });
+}
+
+/**
+ * Refresh a onOffSwitcher for a Piece added with addOnOffSwitcher(...)
+ * @param {Piece} piece
+ * @returns {void}
+ */
+function refreshPieceVolet( piece ){
+        //change imgState
+        var eltImgState = $("#onOffRow_" + piece.id + " .imgState");
+        if( !eltImgState.hasClass("no") ){
+            eltImgState.removeClass( piece.voletOuvert ? "off" : "on" );
+            eltImgState.addClass( piece.voletOuvert ? "on" : "off" );
+        }
+        
+        var eltBtnOn = $("#onOff_"+piece.id+"On");
+        var eltBtnOff = $("#onOff_"+piece.id+"Off");
+        if( eltBtnOn.hasClass("hidden") || eltBtnOff.hasClass("hidden") ){
+            eltBtnOn.removeClass("hidden");
+            eltBtnOff.removeClass("hidden");
+            eltBtnOn.addClass( piece.voletOuvert ? "hidden" : "" );
+            eltBtnOff.addClass( piece.voletOuvert ? "" : "hidden" );
+        }
+    }
